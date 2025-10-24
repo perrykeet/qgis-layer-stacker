4f87758c39824422cf2bd810b6fac7ae4967563e
perrykeet <cookaburger@gmail.com>
2025-10-24 15:52:28 +0200

    Add clean_dsm_resampled.ps1 to change default WebODM DSM border pixels from -9999 to 0 and to add NoData = 0

---
+++ clean_dsm_resampled.ps1
@@ -0,0 +1,21 @@
+# clean_dsm_resampled.ps1
+# Converts -9999 border pixels in DSM to 0
+
+# --- CONFIGURE PATHS ---
+$python = "C:\QGIS 3.40.10\apps\Python312\python.exe"
+$gdal_calc = "C:\QGIS 3.40.10\apps\Python312\Scripts\gdal_calc.py"
+
+$inputDSM = Join-Path $PSScriptRoot "Kummen20250618_dsm_resampled.tif"
+$outputDSM = Join-Path $PSScriptRoot "Kummen20250618_dsm_resampled_clean.tif"
+
+# --- RUN CALCULATION ---
+Write-Host "Converting -9999 border pixels to 0 for DSM ..."
+& $python $gdal_calc `
+  -A $inputDSM `
+  --outfile=$outputDSM `
+  --calc="numpy.where(A==-9999,0,A)" `
+  --NoDataValue=0 `
+  --type=Float32 `
+  --overwrite
+
+Write-Host "Done! Cleaned DSM saved as $outputDSM"
